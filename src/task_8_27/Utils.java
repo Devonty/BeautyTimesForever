7c0950bbfe28c629ded49d53896ede2394546309
elfimov_a_m <smurf20044@mail.ru>
2022-11-09 15:11:43 +0300

    task_8 utils fixed

--- src/task_8_27/Utils.java
+++ src/task_8_27/Utils.java
@@ -1,12 +1,13 @@
 package task_8_27;
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.List;
 
 public class Utils {
-
     public static void writeToFile(String fileName, String data) {
         try {
             BufferedWriter writer = new BufferedWriter(new FileWriter(fileName));
@@ -18,4 +19,52 @@ public class Utils {
             ie.printStackTrace();
         }
     }
+
+    public static String readFile(String fileName) {
+        StringBuilder fileDataBuilder = new StringBuilder();
+        InputStream in = Windows.class.getResourceAsStream("data/input/" + fileName);
+        try {
+            if (in != null) {
+                BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+                String line = null;
+                while ((line = reader.readLine()) != null) {
+                    fileDataBuilder.append(line).append('\n');
+                }
+            } else {
+                fileDataBuilder.append("//_File_not_found_//");
+            }
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+
+        return fileDataBuilder.toString();
+    }
+
+    public static List<List<Integer>> getIntData(String fileData) {
+        List<List<Integer>> listOfLists = new ArrayList<>();
+        String[] lines = fileData.split("\n");
+
+        for (String line : lines) {
+            List<Integer> listInteger = new ArrayList<>();
+            String[] lineSplited = line.split(" ");
+
+            for (String strNum : lineSplited) {
+                try {
+                    Integer num = Integer.parseInt(strNum);
+                    listInteger.add(num);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+            listOfLists.add(listInteger);
+        }
+
+        return listOfLists;
+    }
+
+    public static String readFile() {
+        return readFile("input.txt");
+    } // Значение по умолчанию
+
+
 }
